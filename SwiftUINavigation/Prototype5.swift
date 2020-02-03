946104fb1370d5cb20c3bb25053447b7809485f7
Dariusz Rybicki <darrarski@gmail.com>
2020-02-03 23:11:58 +0100

    Add NavigationItem protocol and Store class

--- SwiftUINavigation/Prototype5.swift
+++ SwiftUINavigation/Prototype5.swift
@@ -17,39 +17,49 @@ struct ContentView_Previews: PreviewProvider {
 
 // MARK: -
 
-struct RootState {}
+struct RootState: NavigationItem {}
 
 struct RootView: View {
+  @EnvironmentObject var store: Store
   let state: RootState
   var body: some View {
     VStack(spacing: 16) {
       Text("Root").font(.title)
       Button(action: {
-        // TODO: navigate to first step
+        self.store.navigation.append(StepState(step: 1))
       }) { Text("Go to first step →") }
     }
   }
 }
 
-struct StepState {
+struct StepState: NavigationItem {
   let step: Int
 }
 
 struct StepView: View {
+  @EnvironmentObject var store: Store
   let state: StepState
   var body: some View {
     VStack(spacing: 16) {
       Text("Step #\(state.step)").font(.title)
       if state.step < 3 {
         Button(action: {
-          // TODO: navigate to next step
+          self.store.navigation.append(StepState(step: self.state.step + 1))
         }) { Text("Go to next step →") }
       } else {
         Text("Done")
       }
       Button(action: {
-        // TODO: navigate to root
+        self.store.navigation.removeLast(self.store.navigation.count - 1)
       }) { Text("← Go back to root") }
     }
   }
+}
+
+// MARK: -
+
+protocol NavigationItem {}
+
+class Store: ObservableObject {
+  @Published var navigation: [NavigationItem] = []
 }
