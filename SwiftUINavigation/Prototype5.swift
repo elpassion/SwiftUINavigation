cc041edaf0a93880387454620ba8bd0a8b3cc1d2
Dariusz Rybicki <darrarski@gmail.com>
2020-02-03 23:06:29 +0100

    Add views and state models

--- SwiftUINavigation/Prototype5.swift
+++ SwiftUINavigation/Prototype5.swift
@@ -16,3 +16,40 @@ struct ContentView_Previews: PreviewProvider {
 #endif
 
 // MARK: -
+
+struct RootState {}
+
+struct RootView: View {
+  let state: RootState
+  var body: some View {
+    VStack(spacing: 16) {
+      Text("Root").font(.title)
+      Button(action: {
+        // TODO: navigate to first step
+      }) { Text("Go to first step →") }
+    }
+  }
+}
+
+struct StepState {
+  let step: Int
+}
+
+struct StepView: View {
+  let state: StepState
+  var body: some View {
+    VStack(spacing: 16) {
+      Text("Step #\(state.step)").font(.title)
+      if state.step < 3 {
+        Button(action: {
+          // TODO: navigate to next step
+        }) { Text("Go to next step →") }
+      } else {
+        Text("Done")
+      }
+      Button(action: {
+        // TODO: navigate to root
+      }) { Text("← Go back to root") }
+    }
+  }
+}
