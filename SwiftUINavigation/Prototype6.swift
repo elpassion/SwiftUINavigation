b3b726bda42989b2b8b1fd192c1e6943aa90f020
Dariusz Rybicki <darrarski@gmail.com>
2020-02-13 00:06:08 +0100

    Prepare for the next prototype

---
+++ SwiftUINavigation/Prototype6.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+import UIKit
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
+#endif
